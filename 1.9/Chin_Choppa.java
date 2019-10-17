72184f4981a3ccc2b668c269c3ee5c2a8e1184e5
Vanish <ivan.ponosov.2002@mail.ru>
2019-10-17 17:26:57 +0300

    HOCHU PROGRAMMU

---
+++ 1.9/Chin_Choppa.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Chin_Choppa {
+    public static void main (String[] args){
+        Scanner in= new Scanner(System.in);
+        double a= in.nextDouble();
+        System.out.println("EGOR SKIN PROGRAMMU"+" "+a+" "+  "POZALYISTA,PES");
+
+    }
+}
